81266e90c810d22a60c888fa414d04b3c716bb3e
Emanuel Bennici <benniciemanuel78@gmail.com>
2019-07-29 22:21:19 +0200

    Remove unused Function captureOutput()
    Signed-off-by: Emanuel Bennici <benniciemanuel78@gmail.com>

--- main_test.go
+++ main_test.go
@@ -175,6 +175,7 @@ func TestMain(t *testing.T) {
 
 /// >>>>> Needed Functions <<<<<
 
+
 // Source: https://gist.github.com/turtlemonvh/e4f7404e28387fadb8ad275a99596f67#file-equal_json-go-L11
 func equalJSON(s1, s2 string) (bool, error) {
 	var o1 interface{}
@@ -193,14 +194,4 @@ func equalJSON(s1, s2 string) (bool, error) {
 	}
 
 	return reflect.DeepEqual(o1, o2), nil
-}
-
-func captureOutput(f func()) string {
-	var buf bytes.Buffer
-	log.SetOutput(&buf)
-
-	f()
-
-	log.SetOutput(os.Stderr)
-	return buf.String()
 }
